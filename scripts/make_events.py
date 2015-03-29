db30dd0f88a2aea5dc6ef990c1c17a548446e08e
teonlamont <teon.brooks@gmail.com>
2015-03-29 11:54:26 +0200

    Update make ica, Clean up

--- scripts/make_events.py
+++ scripts/make_events.py
@@ -4,24 +4,30 @@ import os.path as op
 
 
 subject = 'A0149'
-path = '/Volumes/teon-backup/Experiments/E-MEG/data/%s/mne/' % subject
-raw_file = '%s_OLDT_calm-raw.fif' % subject
+path = '/Users/teon/Google Drive/E-MEG/data/%s/mne/' % subject
+raw_file = '%s_OLDT_calm_lp40-raw.fif' % subject
 raw = mne.io.Raw(op.join(path, raw_file), verbose=False, preload=False)
 
 evts = mne.find_stim_steps(raw)
-exp_idx = np.array([(x & 2 ** 5) >> 5 for x in evts[:, 2]], dtype=bool)
-evts = evts[exp_idx]
+exp = np.array([(x & 2 ** 5) >> 5 for x in evts[:, 2]], dtype=bool)
+evts = evts[exp]
 idx = np.where(evts[:, 1] == 0)[0]
 idy = np.nonzero(evts[:, 2])[0]
 idx = np.intersect1d(idx, idy)
 evts = evts[idx]
 triggers = evts[:, 2]
 
-
-priming_idx = np.array([(x & 2 ** 4) >> 4 for x in triggers], dtype=bool)
+semantic = np.array([(x & 2 ** 4) >> 4 for x in triggers], dtype=bool)
 nonword_pos = np.array([(x & (2 ** 3 + 2 ** 2)) >> 2 for x in triggers])
 current_pos = np.array([(x & (2 ** 1 + 2 ** 0)) >> 0 for x in triggers])
-words_idx = np.where(nonword_pos - current_pos != 0)[0]
+
+idx = np.where(nonword_pos - current_pos != 0)[0]
+idy = np.where(current_pos < nonword_pos)[0]
+idy2 = np.where(nonword_pos == 0)[0]
+idy = np.unique(np.hstack((idy, idy2)))
+
+semantic_idx = np.where(semantic)[0]
+words_idx = np.intersect1d(idx, idy)
 nonwords_idx = np.where(nonword_pos - current_pos == 0)[0]
 primes_idx = np.intersect1d(np.where(current_pos == 1)[0], words_idx)
 targets_idx = np.intersect1d(np.where(current_pos == 2)[0], words_idx)
@@ -35,13 +41,21 @@ words = words[idx]
 
 # semantic priming condition
 priming = evts
-primed_idx = np.intersect1d(np.where(priming_idx)[0], targets_idx)
+primed_idx = np.intersect1d(semantic_idx, targets_idx)
 priming[primed_idx, 2] = 4
 unprimed_idx = np.setdiff1d(targets_idx, primed_idx)
 priming[unprimed_idx, 2] = 3
 idx = np.hstack((unprimed_idx, primed_idx))
 priming = priming[idx]
-evts = np.vstack((words, priming))
+
+# prime vs target
+pos = evts
+pos[primes_idx, 2] = 5
+pos[targets_idx, 2] = 6
+idx = np.hstack((primes_idx, targets_idx))
+pos = pos[idx]
+
+evts = np.vstack((words, priming, pos))
 idx = zip(evts[:, 0], np.arange(evts.size))
 idx = list(zip(*sorted(idx))[-1])
 evts = evts[idx]
