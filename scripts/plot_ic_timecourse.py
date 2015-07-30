21a2ae36a3ff25a1fa75959527d74ae3e8d216af
Teon Brooks <teon.brooks@gmail.com>
2015-07-30 19:53:00 -0400

    Updates
    Updated the plot_ic_timecourse script.

--- scripts/plot_ic_timecourse.py
+++ scripts/plot_ic_timecourse.py
@@ -4,9 +4,12 @@ import mne
 import matplotlib.pyplot as plt
 
 path = '/Users/teon/Google Drive/E-MEG/data/A0129/mne/'
+fname_raw = op.join(path, 'A0129_OLDT_calm_iir_filt-raw.fif')
+raw = mne.io.read_raw_fif(fname_raw)
 fname_epo = op.join(path, 'A0129_OLDT_xca_calm_iir_filt-epo.fif')
 epochs = mne.read_epochs(fname_epo)
-before = epochs.average().plot()
+before = epochs.average()
+p_before = before.plot()
 
 ica = mne.preprocessing.ICA(.9, random_state=42, method='infomax')
 ica.fit(epochs)
@@ -15,13 +18,26 @@ ics = ica.get_sources(epochs)
 picks = range(len(ics.info['ch_names']))
 power, itc = mne.time_frequency.tfr_multitaper(ics, np.arange(10) + 1.,
     (np.arange(10) + 1.) /2, picks=picks, return_itc=True)
-itc.crop(-.1, .03, copy=False)
+# itc.crop(-.1, .03, copy=False)
 
-rows = columns = np.ceil(np.sqrt(itc.info['nchan'])).astype(int)
-fig, axes = plt.subplots(rows, columns)
+nchan = itc.info['nchan']
+columns = 5  # match topo plots
+rows = np.ceil(float(nchan) / columns).astype(int)
+fig, axes = plt.subplots(rows, columns, figsize=(20, 20))
+
 axes = [ax for axe in axes for ax in axe]
-
+for ii in range(nchan - (columns*rows)):
+    axes[-(ii + 1)].axis('off')
 itc.plot(picks=picks, axes=axes[:itc.info['nchan']])
+
+fig.savefig('/Applications/packages/E-MEG/output/images/ica-tf.pdf')
+ica.exclude = [2, 30]
+ica_bads = ica.plot_components(ica.exclude)
+ica_bads.savefig('/Applications/packages/E-MEG/output/images/ica-bads.pdf')
+after = ica.apply(before, copy=True)
+p_after = after.plot()
+p_after.savefig('/Applications/packages/E-MEG/output/images/ica-after.pdf')
+p_before.savefig('/Applications/packages/E-MEG/output/images/ica-before.pdf')
 
 # select high ITC pre-onset, but low afterwards
 
