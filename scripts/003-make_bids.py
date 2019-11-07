7f803d7210ba1ee199d2baa50b2438ee18c0ffa0
Teon L Brooks <teon.brooks@gmail.com>
2019-11-07 17:35:06 -0500

    Update 003-make_bids.py

--- scripts/003-make_bids.py
+++ scripts/003-make_bids.py
@@ -1,9 +1,11 @@
 import os.path as op
 import numpy as np
+import shutil as sh
 
 from mne import read_events
 from mne.io import read_raw_fif
-from mne_bids import write_raw_bids, make_bids_basename, make_dataset_description
+from mne_bids import (write_raw_bids, make_bids_basename,
+                      make_bids_folders, make_dataset_description)
 
 import config
 import config_raw
@@ -18,6 +20,7 @@ project_name = config.project_name
 input_path = config.drives['home']
 output_path = op.join(config.drives['home'], '..', '..', project_name)
 
+# meg.
 for subject in config_raw.subjects.keys():
     print(config.banner % subject)
 
@@ -27,13 +30,32 @@ for subject in config_raw.subjects.keys():
     raw = read_raw_fif(fname_raw)
     events_data = read_events(op.join(input_path, subject, 'mne',
                               subject + '_%s-eve.txt' % exp))
-    bids_basename = make_bids_basename(subject=subject, session='01',
-                                       run='01', task=project_name)
+    bids_basename = make_bids_basename(subject=subject, task=project_name)
     write_raw_bids(raw, bids_basename, output_path,
                    event_id=event_id, events_data=events_data,
                    overwrite=True)
 
+# eyetrack. note, the BEP for eyetracking isn't merged
+# but this is likely the naming convention for it
+for subject, experiments in config_raw.subjects.items():
+    exps = experiments[0], experiments[2]
+    subname = 'sub-{}'.format(subject)
+    for ii, exp in enumerate(exps, 1):
+        if exp != 'n/a':
+            bids_basename = make_bids_basename(subject=subject,
+                                            run='{:02d}'.format(ii),
+                                            task=project_name)
+            bids_eyetrack = op.join(output_path, subname,
+                                    'eyetrack', bids_basename + '_eyetrack.edf')
+            input_fname = op.join(input_path, subject,
+                                'edf', '{}_{}.edf'.format(subject, exp))
+            make_bids_folders(subject=subject, kind='eyetrack',
+                            output_path=output_path)
+            sh.copyfile(input_fname, bids_eyetrack)
+
+# make a dataset description
 make_dataset_description(path=output_path, data_license='CC-BY',
+                         name=project_name,
                          authors=['Teon L Brooks', 'Laura Gwilliams',
                                   'Alexandre Gramfort', 'Alec Marantz'],
                          how_to_acknowledge='',
