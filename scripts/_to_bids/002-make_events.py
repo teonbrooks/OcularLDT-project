5cf7365f40e30eb70951cc11cb28db46996577b4
Teon L Brooks <teon.brooks@gmail.com>
2022-01-18 19:28:11 -0500

    Reverting back to original pattern
    The files in _to_bids correspond to the files in my original folder pre-BIDS. the script so correspond to those files in particular.

--- scripts/_to_bids/002-make_events.py
+++ scripts/_to_bids/002-make_events.py
@@ -26,8 +26,8 @@ filt = config.filt
 for subject in config.subjects:
     print(config.banner % subject)
 
-    fname_template = op.join(path, subject, 'meg', 'sub-{}_task-{}'.format(subject, exp))
-    fname_evts = fname_template + '-events.tsv'
+    fname_template = op.join(path, subject, 'mne', '_'.join((subject, exp)))
+    fname_evts = fname_template + '-eve.txt'
     fname_raw = fname_template + '_calm_' + filt + '_filt-raw.fif'
     # write the co-registration file
     fname_trial = fname_template + '_meg_trial_struct.txt'
