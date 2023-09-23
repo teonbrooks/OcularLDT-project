e88a7a3fa1776a91108409360b5b57f3beddace8
Teon L Brooks <teon.brooks@gmail.com>
2023-09-23 14:25:15 -0400

    Create 01_make_eyetrack_datafile.py, _recode_coreg_events.py
    This file creates the basis for behavioral analyses and the coregistration needed for synchronizing with MEG data.
    
    Contains the recoding function for triggers. We should consider merging it with the original version in `_to_bids` to be DRYer

---
+++ scripts/behavior/01_make_eyetrack_datafile.py
@@ -0,0 +1,38 @@
+import os.path as op
+import json
+
+from _recode_coreg_events import _recode_events
+from _reading import AOIReport
+
+
+cfg = json.load(open(op.join('/', 'Users', 'teonbrooks', 'codespace',
+                             'OcularLDT-project', 'scripts', 'config.json')))
+# Define OLDT interest areas
+fname_ia = op.join(cfg['project_path'], 'input', 'stimuli',
+                   'OcularLDT_ias.txt')
+ia_words = ['fixation', 'prime', 'target', 'post']
+
+
+for subject, experiments in cfg['exp_list'].items():
+    print(cfg['banner'] % subject)
+    # Define output
+    subject_ds = list()
+    n_trials = 0
+    for ii, exp in enumerate(experiments, 1):
+        if exp == 'n/a':
+            continue
+        # Define filenames
+        basename = op.join(cfg['bids_root'], f"sub-{subject}", 'eyetrack',
+                       f"sub-{subject}_task-{cfg['task']}_run-{ii:02d}")    
+        fname_edf = basename + "_eyetrack.edf"
+        fname_ds = basename + "_log.tsv"
+
+        # extracting fixation times from the edf file.
+        data = AOIReport(fname_edf, fname_ia, ia_words).data
+
+        # we need to recode before we save to disk
+        data['trigger'] = data['msg'].str.extract(r'TTL ([0-9]+)')
+        data['trigger'] = data['trigger'].fillna(0).astype(int)
+        data['trigger'] = _recode_events(data['trigger'])
+
+        data.to_csv(fname_ds, sep='\t', index=False)
---
+++ scripts/behavior/_recode_coreg_events.py
@@ -0,0 +1,79 @@
+"""
+TODO: this script is not DRY (cf. ../_to_bids/_recode_events.py)
+Consider a consolidation, perhaps a helper file.
+
+Recoding Events
+---------------
+To deal with the way MNE deals with events with multiple tagging, we
+devised a way to handle this by repacking tag info in a binary fashion.
+Maybe next time I will code my events just directly as such
+(consult schematic in lab notebook).
+
+Prime += 1
+Target += 2
+Priming += 4
+Nonword += 8
+
+This recoding efficiency allows for Hierarchial Event Descriptors
+(HED tags).
+"""
+import numpy as np
+
+
+def _recode_events(triggers):
+    
+    expt_idx = np.array([(x & 2 ** 5) >> 5 for x in triggers], dtype=bool)
+
+    # unpack trigger information
+    semantic = np.array([(x & 2 ** 4) >> 4 for x in triggers],
+                        dtype=bool)
+    nonword_pos = np.array([(x & (2 ** 3 + 2 ** 2)) >> 2
+                            for x in triggers])
+    current_pos = np.array([(x & (2 ** 1 + 2 ** 0)) >> 0
+                            for x in triggers])
+
+    idx = np.where(nonword_pos - current_pos != 0)[0]
+    # word region
+    idy = np.where(np.logical_and(current_pos > 0,
+                                  current_pos < nonword_pos))[0]
+    # nonword is absent
+    idy2 = np.where(nonword_pos == 0)[0]
+    # not fixation cross
+    idy3 = np.where(current_pos != 0)[0]
+    idy = np.unique(np.hstack((idy, idy2, idy3)))
+    # find nonwords
+    n_idx = np.where(nonword_pos - current_pos == 0)[0]
+    n_idy = np.where(nonword_pos != 0)[0]
+
+    # nonwords vs words
+    words_idx = np.intersect1d(idx, idy)
+    nonwords_idx = np.intersect1d(n_idx, n_idy)
+    # fixation
+    fix_idx = np.where(current_pos == 0)[0]
+    # prime vs target
+    primes_idx = np.where(current_pos == 1)[0]
+    targets_idx = np.where(current_pos == 2)[0]
+    # semantic
+    semantic_idx = np.where(semantic)[0]
+
+    ############
+    # Recoding #
+    ############
+    recodeds = np.zeros(triggers.shape[0], int)
+    # prime vs target
+    recodeds[primes_idx] += 1
+    recodeds[targets_idx] += 2
+    # semantic priming
+    recodeds[semantic_idx] += 4
+    # word vs nonword
+    recodeds[nonwords_idx] += 8
+    # fixation
+    recodeds[fix_idx] = 128
+    # make all practice trials zero
+    recodeds[~expt_idx] = 0
+
+    # new_trig = {'fix_pos': current_pos,
+    #             'old_trig': triggers,
+    #             'trig': recodeds}
+    
+    return recodeds
